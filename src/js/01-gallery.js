07b025354e02c8ff3aa068cbe68251861ed69371
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-21 21:36:33 +0200

    add file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,2 @@
-
+import throttle from 'lodash.throttle';
+console.log(throttle);
