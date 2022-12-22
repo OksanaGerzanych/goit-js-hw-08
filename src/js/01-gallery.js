088557dabbac9b6977af0a19b6398d17edeeb47e
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-22 15:23:14 +0200

    add file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-import SimpleLightbox from "simplelightbox";
-
 console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+
+
+const markup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ description, original, preview }) => {
+    return `
+    <div class="gallery__item"><a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+   </a></div>`
+    }).join('');
+ 
+}
+gallery.insertAdjacentHTML("beforeend", markup);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition:'bottom', captionDelay: 250 });
