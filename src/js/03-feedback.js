578735ef60dcc658a38b403a34826277d9e7170f
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-25 17:12:49 +0200

    add file

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,90 @@
+// import throttle from 'lodash.throttle';
+
+// const STORAGE_KEY = 'feedback-form-state';
+
+// const getFormValue = form => ({ email: form.elements.email.value, message: form.elements.message.value });
+
+// function onFormSubmit(event) {
+  
+//   event.preventDefault();
+
+//   console.log(getFormValue(event.currentTarget));
+
+//   localStorage.removeItem(STORAGE_KEY);
+//   event.currentTarget.reset();
+// }
+
+// function doIt() {
+
+//   const form = document.querySelector('.feedback-form');
+
+//   if (!form) {
+//     console.log('Error: invalid markup!');
+//     return;
+//   }
+
+//   const dataForm = localStorage.getItem(STORAGE_KEY);
+//   if (dataForm) {
+//     try {
+//       const data = JSON.parse(dataForm);
+//       form.elements.email.value   = data.email;
+//       form.elements.message.value = data.message;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+//   form.addEventListener('input', throttle(event =>
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(getFormValue(form))), 500));
+  
+//   form.addEventListener('submit', onFormSubmit);
+// }
+
+// doIt();
+
 import throttle from 'lodash.throttle';
+ const form = document.querySelector(".feedback-form");
+const message= document.querySelector(".feedback-form textarea");
+const email = document.querySelector(".feedback-form input")
+const STORAGE_KEY = "feedback-form-state";
 
-const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
 
-const getFormValue = form => ({ email: form.elements.email.value, message: form.elements.message.value });
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+
+localStorage.setItem(STORAGE_KEY, JSON.stringify({ email: email.value, message: message.value }));
+}
 
 function onFormSubmit(event) {
-  
-  event.preventDefault();
-
-  console.log(getFormValue(event.currentTarget));
-
-  localStorage.removeItem(STORAGE_KEY);
-  event.currentTarget.reset();
+    event.preventDefault();
+    console.log({ email: email.value, message: message.value });
+    localStorage.removeItem(STORAGE_KEY);
+    event.currentTarget.reset();
 }
 
-function doIt() {
-
-  const form = document.querySelector('.feedback-form');
-
-  if (!form) {
-    console.log('Error: invalid markup!');
-    return;
+try {
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (formData !== null) {
+    email.value = formData.email;
+    message.value = formData.message;
   }
-
-  const dataForm = localStorage.getItem(STORAGE_KEY);
-  if (dataForm) {
-    try {
-      const data = JSON.parse(dataForm);
-      form.elements.email.value   = data.email;
-      form.elements.message.value = data.message;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  form.addEventListener('input', throttle(event =>
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(getFormValue(form))), 500));
-  
-  form.addEventListener('submit', onFormSubmit);
+}catch (error) {
+  console.log(error);
 }
 
-doIt();
+
+// dataFormSave();
+
+// try {
+//     lastSession = JSON.parse(localStorage.getItem('feedback-form-state'));
+//     if(lastSession!==null){
+//         email.value=lastSession.email;
+//         message.value=lastSession.message;
+//      }
+// } catch (err) {
+//     console.log('Parse error:' + err.message);
+// }
