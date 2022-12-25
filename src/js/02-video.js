10b94608d5c436959a01214a1d17d71b3a9724dd
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-25 12:53:51 +0200

    add file

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,18 +2,17 @@ import Player from '@vimeo/player';
 import  throttle  from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
+const CURRENT_TIME = 'videoplayer-current-time';
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
- localStorage.setItem('videoplayer-current-time', seconds);
- }
+localStorage.setItem(CURRENT_TIME, seconds);
+}
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-    .catch(function (error) {
-    console.error(error)
-});
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME))
+ 
 
 
 
