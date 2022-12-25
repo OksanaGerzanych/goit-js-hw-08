03c6baef899e77ea75102a54297e49233cc5e06d
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-25 17:21:07 +0200

    add file

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,61 +1,16 @@
-// import throttle from 'lodash.throttle';
-
-// const STORAGE_KEY = 'feedback-form-state';
-
-// const getFormValue = form => ({ email: form.elements.email.value, message: form.elements.message.value });
-
-// function onFormSubmit(event) {
-  
-//   event.preventDefault();
-
-//   console.log(getFormValue(event.currentTarget));
-
-//   localStorage.removeItem(STORAGE_KEY);
-//   event.currentTarget.reset();
-// }
-
-// function doIt() {
-
-//   const form = document.querySelector('.feedback-form');
-
-//   if (!form) {
-//     console.log('Error: invalid markup!');
-//     return;
-//   }
-
-//   const dataForm = localStorage.getItem(STORAGE_KEY);
-//   if (dataForm) {
-//     try {
-//       const data = JSON.parse(dataForm);
-//       form.elements.email.value   = data.email;
-//       form.elements.message.value = data.message;
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-
-//   form.addEventListener('input', throttle(event =>
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(getFormValue(form))), 500));
-  
-//   form.addEventListener('submit', onFormSubmit);
-// }
-
-// doIt();
-
 import throttle from 'lodash.throttle';
- const form = document.querySelector(".feedback-form");
+
+const form = document.querySelector(".feedback-form");
 const message= document.querySelector(".feedback-form textarea");
 const email = document.querySelector(".feedback-form input")
 const STORAGE_KEY = "feedback-form-state";
 
 let formData = {};
 
-
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormInput(event) {
-
 localStorage.setItem(STORAGE_KEY, JSON.stringify({ email: email.value, message: message.value }));
 }
 
@@ -77,14 +32,3 @@ try {
 }
 
 
-// dataFormSave();
-
-// try {
-//     lastSession = JSON.parse(localStorage.getItem('feedback-form-state'));
-//     if(lastSession!==null){
-//         email.value=lastSession.email;
-//         message.value=lastSession.message;
-//      }
-// } catch (err) {
-//     console.log('Parse error:' + err.message);
-// }
